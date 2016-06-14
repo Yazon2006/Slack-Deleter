0d48398aedcae670f7442da59e5fde652428b850
Yazon2006 <yazon2006@yandex.ru>
2016-06-14 12:24:16 +0300

    all work as expected

--- src/main/java/com/deleter/SlackDeleter.java
+++ src/main/java/com/deleter/SlackDeleter.java
@@ -2,7 +2,6 @@ package com.deleter;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import rx.Observable;
@@ -13,13 +12,11 @@ public class SlackDeleter {
     private static WebDriverWait wait = new WebDriverWait(driver, 36000);
 
     public static void main(String[] args) {
-//        setupWebDriver();
-
         String url = "https://jelvix.slack.com/";
 
         driver.get(url);
 
-        waitAndClick(By.id("flex_menu_toggle"));
+        waitAndClick(By.xpath("//*[@id=\"flex_menu_toggle\"]"));
         waitAndClick(By.id("files_user"));
 
         Observable.just(wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("file_listing_bottom_button"))))
@@ -32,34 +29,19 @@ public class SlackDeleter {
         }
     }
 
-//    private static void setupWebDriver() {
-//        try {
-//            driver = new ChromeDriver();
-//        } catch (Exception e1) {
-//            System.out.println("There are no ChromeDriver");
-//            try {
-//                driver = new OperaDriver();
-//            } catch (Exception e2) {
-//                System.out.println("There are no OperaDriver");
-//                try {
-//                    driver = new FirefoxDriver();
-//                } catch (Exception e3) {
-//                    System.out.println("There are no FirefoxDriver");
-//                    return;
-//                }
-//            }
-//        }
-//
-//    }
-
     private static void waitAndClick(By by) {
         Observable.just(wait.until(ExpectedConditions.visibilityOfElementLocated(by)))
-                .subscribe(WebElement::click, SlackDeleter::handleError);
+                .subscribe((webElement) -> {
+                    if (Properties.getInstance().getBrowser().equals(Properties.Browsers.CHROME)) {
+                        waitForJsListener();
+                    }
+                    webElement.click();
+                }, SlackDeleter::handleError);
     }
 
     private static void deleteFile() {
         waitAndClick(By.xpath("//div[starts-with(@id,'file_')]"));
-        waitAndClick(By.id("file_action_cog"));
+        waitAndClick(By.xpath("//*[@id=\"file_action_cog\"]"));
         waitAndClick(By.id("delete_file"));
         waitAndClick(By.xpath("//*[@id='generic_dialog']/div[3]/a[3]"));
         waitAndClick(By.xpath("//*[@id=\"page_contents\"]/div[1]/a[2]"));
@@ -67,6 +49,14 @@ public class SlackDeleter {
 
     private static void handleError(Throwable throwable) {
         throwable.printStackTrace();
+    }
+
+    private static void waitForJsListener() {
+        try {
+            Thread.sleep(6000);                 //1000 milliseconds is one second.
+        } catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
     }
 
 }
